8d30625048cac980a550627ae26c851ff0da5d61
Suleyman Farajli <suleyman@farajli.net>
2024-07-23 12:07:09 +0400

    nvim config removed

--- config/nvim/init.lua
+++
@@ -1,2 +0,0 @@
--- Suleyman's gruvbox themed neovim config at "https://github.com/thei0n/slcf"
-require("master")
--- config/nvim/lua/master/plugins/nvim-autopairs.lua
+++
@@ -1,6 +0,0 @@
-require('nvim-autopairs').setup({
-	 disable_filetype = { "TelescopePrompt", "spectre_panel", "neo-tree-popup", "conf"},
-	 disable_in_macro = true,
-	 disable_in_visualblock = false,
-	 disable_in_replace_mode = true,
-})
