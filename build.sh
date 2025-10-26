5e385e121ca1fe9428b23bb80d30c61bd9870e32
Suleyman Farajli <suleyman@farajli.net>
2025-10-26 20:38:18 +0400

    fix(build.sh): ShellCheck warnings and quote variable expansions in build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ dmenu_version="dmenu_farajli-5.3.0"
 slstatus_version="slstatus_farajli-1.0.1"
 st_version="st_farajli-0.9.2.1"
 
-MAKEDIR() { mkdir -pv "${1}" }
+MAKEDIR() { mkdir -pv "${1}"; }
 
 COPY() {
 	cp -r  "${1}" "${2}"
@@ -28,7 +28,7 @@ pkg() {
 		"unpack") tar xf "${package}".tar.gz ;;
 		"compile") make -C "${package}" ;;
 		"install") PREFIX=${BINDIR} make -C "${package}" install ;;
-		"font_install")	cp -r font $(FONTDIR) ;;
+		"font_install")	cp -r font "${FONTDIR}" ;;
 		?)
 			echo Invalid usage of pkg >&2
 			return 1
@@ -44,24 +44,24 @@ create_dirs() {
 	MAKEDIR "${FONTDIR}"
 }
 
-install_bash()        { COPY config/bash/bashrc              "${BASHRC}"  }
-install_dunst()       {	COPY config/dunst                    "${CONFDIR}" }
-install_lf()          {	COPY config/lf                       "${CONFDIR}" }
-install_mimeapps()    {	COPY config/mimeapps.list            "${CONFDIR}" }
-install_mpv()         {	COPY config/mpv                      "${CONFDIR}" }
-install_nsxiv()       {	COPY config/nsxiv                    "${CONFDIR}" }
-install_nvim()        {	COPY config/nvim                     "${CONFDIR}" }
-install_picom()       { COPY config/picom                    "${CONFDIR}" }
-install_qutebrowser() {	COPY config/qutebrowser              "${CONFDIR}" }
-install_sxiv()        {	COPY config/sxiv                     "${CONFDIR}" }
-install_vim()         {	COPY config/vim                      "${CONFDIR}" }
-install_x11()         {	COPY config/x11                      "${CONFDIR}" }
-install_zathura()     {	COPY config/zathura                  "${CONFDIR}" }
-install_wallpapers()  { COPY config/wallpapers               "${DATADIR}" }
-install_pacman()      { sudo cp distros/arch-linux/pacman.conf /etc       }
+install_bash()        { COPY config/bash/bashrc              "${BASHRC}";  }
+install_dunst()       {	COPY config/dunst                    "${CONFDIR}"; }
+install_lf()          {	COPY config/lf                       "${CONFDIR}"; }
+install_mimeapps()    {	COPY config/mimeapps.list            "${CONFDIR}"; }
+install_mpv()         {	COPY config/mpv                      "${CONFDIR}"; }
+install_nsxiv()       {	COPY config/nsxiv                    "${CONFDIR}"; }
+install_nvim()        {	COPY config/nvim                     "${CONFDIR}"; }
+install_picom()       { COPY config/picom                    "${CONFDIR}"; }
+install_qutebrowser() {	COPY config/qutebrowser              "${CONFDIR}"; }
+install_sxiv()        {	COPY config/sxiv                     "${CONFDIR}"; }
+install_vim()         {	COPY config/vim                      "${CONFDIR}"; }
+install_x11()         {	COPY config/x11                      "${CONFDIR}"; }
+install_zathura()     {	COPY config/zathura                  "${CONFDIR}"; }
+install_wallpapers()  { COPY config/wallpapers               "${DATADIR}"; }
+install_pacman()      { sudo cp distros/arch-linux/pacman.conf /etc;       }
 
 install_git() {
-	sed '/# signingkey = <to be set manually>/d' config/git/gitconfig > $(GITCONFIG)
+	sed '/# signingkey = <to be set manually>/d' config/git/gitconfig > "${GITCONFIG}"
 	printf '%s\n' \
 	"#####################################################################" \
 	"Warning: Git commit/tag signing is enabled but signingKey is not set." \
@@ -77,7 +77,7 @@ install_sites() {
 }
 
 install_zsh() {
-	MAKEDIR                              $(dirname "${ZCACHE}")
+	MAKEDIR                              "$(dirname "${ZCACHE}")"
 	touch                                "${ZCACHE}"
 	COPY config/zsh/zshrc                "${ZSHRC}"
 }
@@ -93,8 +93,8 @@ install_scripts() {
 	COPY scripts/* "${BINDIR}"
 }
 
-install_liberationmono() { pkg LiberationMono sync unpack font_install   }
-install_dwm()            { pkg "${dwm_version}" sync unpack install      }
-install_st()             { pkg "${st_version}" sync unpack install       }
-install_dmenu()          { pkg "${dmenu_version}" sync unpack install    }
-install_slstatus()       { pkg "${slstatus_version}" sync unpack install }
+install_liberationmono() { pkg "LiberationMono" sync unpack font_install; }
+install_dwm()            { pkg "${dwm_version}" sync unpack install;      }
+install_st()             { pkg "${st_version}" sync unpack instal;        }
+install_dmenu()          { pkg "${dmenu_version}" sync unpack install;    }
+install_slstatus()       { pkg "${slstatus_version}" sync unpack install; }
