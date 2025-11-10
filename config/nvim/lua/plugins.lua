6bdf89bd7378bf188998deb59f8089b1e4e94776
Suleyman Farajli <suleyman@farajli.net>
2025-11-10 13:20:32 +0400

    feat(nvim): start unfolded in org files

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -54,6 +54,7 @@ require("nvim-autopairs").setup({
 require('orgmode').setup({
 	org_agenda_files = '~/orgfiles/**/*',
 	org_default_notes_file = '~/orgfiles/refile.org',
+	org_startup_folded = 'showeverything'
 })
 
 require 'mdeval'.setup({
